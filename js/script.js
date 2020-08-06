cbfbccfadbd1409ce27c0b8c01ea288274c53fa6
Luiz Eduardo Santos <luiz_eduardoperro@outlook.com>
2020-08-06 10:22:59 -0300

    dinamico

--- js/script.js
+++ js/script.js
@@ -4,4 +4,18 @@ $(function () {
         arrows: false,
         autoplay: true
     });
+
+    // get cases
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8000/cases",
+        data: {token: "mPczSRTGktoOVn5O9CnfuRufmmC9F6bG"},
+        success: function (response) {
+            $('#realizados').html(response.realizados);
+            $('#confirmados').html(response.confirmados);
+            $('#negativos').html(response.negativos);
+            $('#obitos').html(response.obitos);
+            $('#atualizacao').html(response.data);
+        }
+    });
 });
